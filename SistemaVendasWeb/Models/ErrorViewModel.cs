4fbf89428fca506234ac674e91ee1f726894e1ae
Lucas <lucasandim@yahoo.com.br>
2023-01-11 15:16:04 -0400

    projeto

---
+++ SistemaVendasWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVendasWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
